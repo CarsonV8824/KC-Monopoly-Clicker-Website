80a1b9a08b63e0f922235f457911882545bf98e3
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-12 16:52:42 +0000

    added in testing for button

--- src/main.py
+++ src/main.py
@@ -12,7 +12,10 @@ def main():
 @app.route('/get_dice_click_from_js', methods=['POST'])
 def get_dice_click_from_js():
     data = request.get_json()
-    print("Received dice click:", data)
+
+    with open('tests/dice_clicks_from_js.log', 'a') as log_file:
+        log_file.write(f"{data}\n")
+    
     return jsonify({"status": "success", "message": "Dice click received successfully."})
 
 if __name__ == '__main__':
